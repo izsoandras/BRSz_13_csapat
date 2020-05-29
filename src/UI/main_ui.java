28a0a238fa879a8976ff1025c42b2d8dcb0de051
Peregi Dávid <dperegi@gmail.com>
2020-05-29 11:06:08 +0200

    ui

--- src/UI/main_ui.java
+++ src/UI/main_ui.java
@@ -2,6 +2,8 @@ package UI;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -9,23 +11,77 @@ import static model.GameParams.LABYRINTH_HEIGHT;
 import static model.GameParams.LABYRINTH_WIDTH;
 
 public class main_ui extends Application {
+    private Stage mainWindow;
+    private Scene Menu, Settings, Toplist, Game;
 
     static int block_size = 10; //10pixel egy blokk mérete
-
     int width=LABYRINTH_WIDTH;       //blokkok száma
     int height=LABYRINTH_HEIGHT;
 
     @Override
-    public void start(Stage ps){
-        VBox root = new VBox(10);
+    public void start(Stage ps) {
+        mainWindow = ps;
 
-        Scene scene = new Scene(root);
+        constructMenu();
+        constructSettings();
+        constructToplist();
+        constructGame();
 
-        ps.setResizable(false);
-        ps.setScene(scene);
-        ps.setTitle("Snake Game");
-
-        ps.show();
-
+        mainWindow.setScene(Menu);
+        mainWindow.setTitle("Snake Game");
+        mainWindow.show();
     }
+    private void constructMenu(){
+        Label lb = new Label("Menü");
+        Button btnGame =new Button("Single Game");
+        btnGame.setOnAction(e ->{mainWindow.setScene(Game);});
+
+        Button btnLoadGame =new Button("Load Game");
+        btnLoadGame.setOnAction(e ->{mainWindow.setScene(Game);});  //ide még kell egy játék betöltés is
+
+        Button btnSettings =new Button("Settings");
+        btnSettings.setOnAction(e ->{mainWindow.setScene(Settings);});
+
+        Button btnMulti =new Button("Multi player");
+        btnMulti.setOnAction(e ->{mainWindow.setScene(Game);});     //multiplayer kezelés
+
+        Button btnToplist =new Button("Toplista");
+        btnToplist.setOnAction(e ->{mainWindow.setScene(Toplist);});
+
+        VBox root = new VBox();
+        root.getChildren().addAll(lb, btnGame, btnLoadGame, btnSettings, btnMulti, btnToplist);
+        Menu=new Scene(root, 600, 600);
+    }
+    private void constructGame(){
+        Label lb = new Label("Játék");
+        Label Score = new Label("Score: ");      //játék állása kell
+
+        Button btnBack = new Button("Back");
+        btnBack.setOnAction(e->{mainWindow.setScene(Menu);});
+
+        VBox root = new VBox();
+        root.getChildren().addAll(lb, Score, btnBack);
+        Game = new Scene(root, 600,600);
+    }
+    private void constructSettings(){
+        Label lb = new Label("Beállítások");
+
+        Button btnBack = new Button("Back");
+        btnBack.setOnAction(e->{mainWindow.setScene(Menu);});
+
+        VBox root = new VBox();
+        root.getChildren().addAll(lb, btnBack);
+        Settings = new Scene(root, 600,600);
+    }
+    private void constructToplist(){
+        Label lb = new Label("Toplista");
+
+        Button btnBack = new Button("Back");
+        btnBack.setOnAction(e->{mainWindow.setScene(Menu);});
+
+        VBox root = new VBox();
+        root.getChildren().addAll(lb, btnBack);
+        Toplist = new Scene(root, 600,600);
+    }
+
 }
